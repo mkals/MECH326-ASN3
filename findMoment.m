fd6e240973176d29e5bab80474fea58437dc94ff
jamiec-ye <jamiec.ye@gmail.com>
2018-11-16 21:41:41 -0800

    Initial implementation of findShear and findMoment

--- findMoment.m
+++ findMoment.m
@@ -1,3 +1,15 @@
-function [M]=findMoment(F)
-
+% Takes in vector of shear, V, along the shaft and the number of slices,
+% length, taken
+function [Mlist]=findMoment(Vlist, length)
+    % Initialize Vlist vector
+    Mlist = zeros(1, length);
+    Mlist(1) = Vlist(1);
+    for i = 2:length
+        Mlist(i) = Mlist(i-1) + Vlist(i);
+    end
+    
+    x = linspace(0, length, length);
+    
+    % TODO: graph labeling
+    plot(x, Mlist)
 end
--- findShear.m
+++ findShear.m
@@ -1,3 +1,15 @@
-function [V]=findShear(F)
-
+% Takes in vector of forces, F, along the shaft and the number of slices,
+% length, taken
+function [Vlist]=findShear(Flist, length)
+    % Initialize Vlist vector
+    Vlist = zeros(1, length);
+    Vlist(1) = Flist(1);
+    for i = 2:length
+        Vlist(i) = Vlist(i-1) + Flist(i);
+    end
+    
+    x = linspace(0, length, length);
+    
+    % TODO: graph labeling
+    plot(x, Vlist)
 end
